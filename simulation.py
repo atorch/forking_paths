4254bda03df95f0d4f3409ecb855d2e67da0a359
Adrian Torchiana <adriantorchiana@granular.ag>
2021-08-25 02:29:12 -0700

    Coverage for regression coefficient CI

--- simulation.py
+++ simulation.py
@@ -5,10 +5,12 @@ from sklearn.linear_model import LinearRegression
 
 def simulate_df(true_beta0, n_obs=500):
 
-    df = pd.DataFrame({
-        "x0": np.random.uniform(size=n_obs),
-        "x1": np.random.uniform(size=n_obs) + np.random.uniform(size=n_obs),
-    })
+    df = pd.DataFrame(
+        {
+            "x0": np.random.uniform(size=n_obs),
+            "x1": np.random.uniform(size=n_obs) + np.random.uniform(size=n_obs),
+        }
+    )
 
     df["x2"] = df["x0"] + df["x1"] + np.random.normal(size=n_obs)
 
@@ -38,16 +40,23 @@ def run_regression_v0(df_train):
 
     residuals = y - model.predict(X)
 
-    X_transpose_X_inverse = np.linalg.inv(np.matmul(np.transpose(X), X))
+    # Note: we need to include the constant when calculating (X^T X)^-1
+    X_with_constant = np.hstack([np.ones((X.shape[0], 1)), X])
 
-    # Note the degrees of freedom correction
-    sigma_squared_hat = np.sum(residuals ** 2) / (X.shape[0] - X.shape[1])
+    X_transpose_X_inverse = np.linalg.inv(
+        np.matmul(np.transpose(X_with_constant), X_with_constant)
+    )
 
-    std_errors = np.sqrt(sigma_squared_hat) * np.diag(X_transpose_X_inverse)
+    # Note the degrees of freedom correction (accounting for the constant term)
+    # See https://web.stanford.edu/~mrosenfe/soc_meth_proj3/matrix_OLS_NYU_notes.pdf
+    sigma_squared_hat = np.sum(residuals ** 2) / (X.shape[0] - X_with_constant.shape[1])
+
+    std_errors = np.sqrt(sigma_squared_hat * np.diag(X_transpose_X_inverse)[1:])
 
     ci_lower = estimated_coefficients - 1.96 * std_errors
     ci_upper = estimated_coefficients + 1.96 * std_errors
 
+    # TODO Also return sigma_squared_hat?
     return estimated_coefficients, ci_lower, ci_upper
 
 
@@ -64,14 +73,29 @@ def main():
 
     true_beta0 = 1.5
 
-    df = simulate_df(true_beta0=true_beta0)
+    n_replications = 300
+    coef_replications = []
+    ci_contains_beta0_replications = []
 
-    coefs, ci_lower, ci_upper = run_regression_v0(df)
+    for _ in range(n_replications):
 
-    ci_contains_beta0 = ci_lower[0] < true_beta0 < ci_upper[0]
+        df = simulate_df(true_beta0=true_beta0)
 
-    # TODO CI coverage
+        coefs, ci_lower, ci_upper = run_regression_v0(df)
+        coef_replications.append(coefs)
+
+        ci_contains_beta0 = ci_lower[0] < true_beta0 < ci_upper[0]
+        ci_contains_beta0_replications.append(ci_contains_beta0)
+
+    ci_coverage = np.mean(ci_contains_beta0_replications)
+    print(f"Coverage of 95% confidence interval for beta_0: {ci_coverage}")
+
+    print(pd.DataFrame(coef_replications))
+    print(pd.DataFrame(coef_replications).describe())
+
     # TODO Plot histogram showing (a) true coefficient value, (b) distribution of coef estimates
+
+    # TODO Another one with publication bias, drawer
 
 
 if __name__ == "__main__":
