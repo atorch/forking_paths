cd72fd9fa8837561e4114bc71d6112122c921fef
Adrian Torchiana <adriantorchiana@granular.ag>
2021-08-25 08:13:47 -0700

    WIP on forking paths simulation

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ References:
 - https://statmodeling.stat.columbia.edu/2014/11/17/power-06-looks-like-get-used/
 - https://statmodeling.stat.columbia.edu/2021/08/20/what-is-a-claim-in-ml-oriented-research-and-when-is-it-not-reproducible/
 
+From Gelman's blog (first link in the list above):
+
+> Statistical analysis shows how it is possible — indeed, easy — to get statistical significance in an uncontrolled study in which rules for data inclusion, data coding, and data analysis are determined after the data have been seen. Simmons, Nelson, and Simonsohn called it “researcher degrees of freedom” and Eric Loken and I called it “the garden of forking paths.” It’s sometimes called “fishing” or “p-hacking” but I don’t like those terms as they can be taken to imply that researchers are actively cheating.
+
+[simulation.py](simulation.py)
--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -14,10 +15,13 @@ def simulate_df(true_beta0, n_obs=500):
 
     df["x2"] = df["x0"] + df["x1"] + np.random.normal(size=n_obs)
 
-    # Note: predictors x3, x4, x5, x6, ... are noise (their true coefficients are zero)
-    for idx in range(3, 10):
+    # Note: predictors x3, x4, x5, x6, ... have true coefficients equal to zero
+    # (they have no effect on y), but they are correlated with x0
+    for idx in range(3, 25):
         colname = f"x{idx}"
-        df[colname] = np.random.uniform(size=n_obs) + np.random.uniform(size=n_obs)
+        df[colname] = (
+            df["x0"] + np.random.uniform(size=n_obs) + np.random.uniform(size=n_obs)
+        )
 
     df["epsilon"] = np.random.normal(size=n_obs)
 
@@ -27,16 +31,7 @@ def simulate_df(true_beta0, n_obs=500):
     return df
 
 
-def run_regression_v0(df_train):
-
-    model = LinearRegression()
-
-    X = df_train[["x0", "x1", "x2"]]
-    y = df_train["y"]
-
-    model.fit(X=X, y=y)
-
-    estimated_coefficients = model.coef_
+def calculate_std_errors(model, y, X):
 
     residuals = y - model.predict(X)
 
@@ -53,18 +48,78 @@ def run_regression_v0(df_train):
 
     std_errors = np.sqrt(sigma_squared_hat * np.diag(X_transpose_X_inverse)[1:])
 
+    return std_errors
+
+
+def run_regression_v0(df_train):
+
+    """
+    This function mimics a researcher doing 'the right thing':
+    they decided ahead of time that they'll run a regression of
+    y on x0, x1 and x2 (and a constant). They run one regression, report
+    their coefficient estimates and confidence intervals, and they're done.
+    """
+
+    model = LinearRegression()
+
+    X = df_train[["x0", "x1", "x2"]]
+    y = df_train["y"]
+
+    model.fit(X=X, y=y)
+
+    # TODO Also return sigma_squared_hat?
+    std_errors = calculate_std_errors(model, y, X)
+
+    estimated_coefficients = model.coef_
     ci_lower = estimated_coefficients - 1.96 * std_errors
     ci_upper = estimated_coefficients + 1.96 * std_errors
 
-    # TODO Also return sigma_squared_hat?
     return estimated_coefficients, ci_lower, ci_upper
 
 
-def run_regression_v1(df_train):
+def run_regression_v1(df_train, beta0_threshold=1.75, max_iterations=20):
 
-    # TODO Keep adding predictors until beta_1 is statistically significantly < 1
+    """
+    This function mimics a researcher following forking paths:
+    they start with a regression of
+    y on x0, x1 and x2 (and a constant), but then examine the estimated
+    coefficient for beta0, check whether it is larger than beta0_threshold,
+    and (if so) keep adding additional predictors and rerunning the regression
+    (for a maximum of max_iterations steps). The analysis -- specifically the set of regression
+    coefficients -- varies based on the data.
+    """
 
-    return estimated_coefficients, ci_lower, ci_upper
+    for last_predictor_idx in range(2, 2 + max_iterations + 1):
+
+        predictors = [f"x{idx}" for idx in range(0, last_predictor_idx + 1)]
+        # print(f"Running regression with predictors {predictors}")
+
+        model = LinearRegression()
+
+        X = df_train[predictors]
+        y = df_train["y"]
+
+        model.fit(X=X, y=y)
+
+        estimated_coefficients = model.coef_
+
+        # Imagine that the researcher has some reason (theory, chances of publication, preconception, etc)
+        # to "want" their estimated beta0 to be larger than beta0_threshold,
+        # or to think that something is wrong with their analysis if their estimate is below the threshold.
+        # They may not be actively cheating, but instead convince themselves that their regression
+        # failed to control for some confounder (which will be added to the set of predictors on the next iteration)
+        if estimated_coefficients[0] > beta0_threshold:
+            break
+
+    # print(f"Stopping forking paths at last_predictor_idx {last_predictor_idx}")
+    std_errors = calculate_std_errors(model, y, X)
+
+    ci_lower = estimated_coefficients - 1.96 * std_errors
+    ci_upper = estimated_coefficients + 1.96 * std_errors
+
+    # Only pay attention to estimated coefficients for beta0, beta1, beta2,
+    # even though the model might include additional predictors
+    return estimated_coefficients[0:3], ci_lower[0:3], ci_upper[0:3]
 
 
 def main():
@@ -73,25 +128,35 @@ def main():
 
     true_beta0 = 1.5
 
-    n_replications = 300
-    coef_replications = []
-    ci_contains_beta0_replications = []
+    for run_regression in [run_regression_v0, run_regression_v1]:
 
-    for _ in range(n_replications):
+        n_replications = 500
+        coef_replications = []
+        ci_contains_beta0_replications = []
 
-        df = simulate_df(true_beta0=true_beta0)
+        for _ in range(n_replications):
 
-        coefs, ci_lower, ci_upper = run_regression_v0(df)
-        coef_replications.append(coefs)
+            df_train = simulate_df(true_beta0=true_beta0)
 
-        ci_contains_beta0 = ci_lower[0] < true_beta0 < ci_upper[0]
-        ci_contains_beta0_replications.append(ci_contains_beta0)
+            coefs, ci_lower, ci_upper = run_regression(df_train)
+            coef_replications.append(coefs)
 
-    ci_coverage = np.mean(ci_contains_beta0_replications)
-    print(f"Coverage of 95% confidence interval for beta_0: {ci_coverage}")
+            ci_contains_beta0 = ci_lower[0] < true_beta0 < ci_upper[0]
+            ci_contains_beta0_replications.append(ci_contains_beta0)
 
-    print(pd.DataFrame(coef_replications))
-    print(pd.DataFrame(coef_replications).describe())
+        print(f"*** results for {run_regression.__name__} ***")
+        ci_coverage = np.mean(ci_contains_beta0_replications)
+        print(f"Coverage of 95% confidence interval for beta_0: {ci_coverage}")
+
+        df = pd.DataFrame(coef_replications, columns=["beta0", "beta1", "beta2"])
+        print(df.describe())
+
+        f, ax = plt.subplots(figsize=(12, 8))
+        ax = df["beta0"].plot.hist(bins=50)
+        plt.savefig(
+            f"sampling_distribution_for_estimated_beta0_{run_regression.__name__}.png"
+        )
+        plt.close()
 
     # TODO Plot histogram showing (a) true coefficient value, (b) distribution of coef estimates
 
